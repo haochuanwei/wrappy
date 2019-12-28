de51234897319ca90dc42a38a61acec7e1d27e5e
Haochuan Wei <haochuanwei@yahoo.com>
2019-12-28 10:35:52 +0000

    Corrected version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wrappy",
-    version="0.1.0",
+    version="0.1.1",
     author="Harry Wei",
     author_email="pepsimixt@gmail.com",
     description="Decorators for common developer utility",
