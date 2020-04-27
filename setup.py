c3b3d4df47ab41f9c13f8465a066cbf533788da7
Harry Wei <haochuanwei@users.noreply.github.com>
2020-04-27 17:50:33 +0800

    RELEASE: 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wrappy",
-    version="0.2.1",
+    version="0.2.2",
     author="Ernie Thornhill",
     author_email="pepsimixt@gmail.com",
     description="Decorators for common developer utility",
