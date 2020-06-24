555dda31c2d0f375c5d9c1b300b8a60e19d522c2
Haochuan Wei <haochuanwei@yahoo.com>
2020-06-24 07:56:17 +0000

    Updated version to 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wrappy",
-    version="0.2.3",
+    version="0.2.5",
     author="Ernie Thornhill",
     author_email="pepsimixt@gmail.com",
     description="Decorators for common developer utility",
